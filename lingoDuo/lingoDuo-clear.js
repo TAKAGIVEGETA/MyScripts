ae4e984800b7b5123f3359a5947db50b77ecbdc5
LZ <125237595+TAKAGIVEGETA@users.noreply.github.com>
2026-03-09 18:22:22 +0800

    upt

--- lingoDuo/lingoDuo-clear.js
+++ lingoDuo/lingoDuo-clear.js
@@ -13,7 +13,7 @@ try {
         if (obj[pool] && obj[pool].length > 0) obj[pool] = [];
     });
   }
-  if (obj.metadata && obj.metadata.type === "duoradio") 
+  if (url.includes("stories") || (obj.metadata && obj.metadata.type === "duoradio"))
     if (obj.elements && obj.elements.length > 0) obj.elements = [];
   $done({ body: JSON.stringify(obj) });
 } catch (e) {
